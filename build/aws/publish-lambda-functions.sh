c59b2f4f4ceb375c670039794f7e83203efeba4c
jban28 <jd.bannister28@gmail.com>
2024-07-04 16:42:16 +0100

    publish-lambda-function.sh working

--- build/aws/publish-lambda-functions.sh
+++ build/aws/publish-lambda-functions.sh
@@ -8,7 +8,18 @@ for func_file in "${lambda_funcs[@]}"; do
     aws iam create-role \
         --role-name "${func_name}_lambda-role" \
         --path "/${API_NAME}/" \
-        --assume-role-policy-document "file://build/aws/lambda-execution-role.json"
+        --assume-role-policy-document "file://build/aws/lambda-execution-role.json" \
+        --query "Role.Arn" \
+        --output "text"
+
+done
+
+# Separate loop with time delay to allow IAM roles to propagate
+sleep 10s
+
+for func_file in "${lambda_funcs[@]}"; do
+    func_name="${func_file/.py/}"
+    func_name="${API_NAME}_${func_name##*/}"
 
     rm lambda_function.zip
     zip -j lambda_function.zip $func_file
@@ -16,7 +27,7 @@ for func_file in "${lambda_funcs[@]}"; do
     aws lambda create-function \
     --function-name "$func_name" \
     --runtime "python3.10" \
-    --role "arn:aws:iam::053630928262:role/${API_NAME}/${func_name}_lambda-role" \
+    --role  "arn:aws:iam::053630928262:role/${API_NAME}/${func_name}_lambda-role" \
     --zip-file "fileb://lambda_function.zip" \
     --handler "${func_name}.lambda_handler" \
     --layers "$layer_arn" \
@@ -36,10 +47,11 @@ done
 
 rm lambda_function.zip
 
+# Function specific configuration
 aws lambda update-function-configuration \
-    --function-name ArtistAPI_auth \
+    --function-name "${API_NAME}_authenticate" \
     --environment "Variables={databaseURI=$DATABASE_URI,rootURL=https://artist-api.s3.amazonaws.com,secretKey=$SECRET_KEY}"
 
 aws lambda update-function-configuration \
-    --function-name ArtistAPI_login_POST \
+    --function-name "${API_NAME}_login_POST" \
     --environment "Variables={databaseURI=$DATABASE_URI,rootURL=https://artist-api.s3.amazonaws.com,secretKey=$SECRET_KEY}"
