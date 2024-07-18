af7309efec3bf1174453d43d90e8813462fd990c
jban28 <jd.bannister28@gmail.com>
2024-07-18 16:35:51 +0100

    add s3 api integration

--- build/aws/setup-apigateway.sh
+++ build/aws/setup-apigateway.sh
@@ -193,35 +193,6 @@ for stage in "${stages[@]}"; do
         --action lambda:InvokeFunction
 done
 
-http_method="PUT"
-path="{id}"
-resource_id=$image_id_resource
-lambda_name="${API_NAME}_image_id_${http_method}"
-aws apigateway put-method \
-    --rest-api-id "$api_id" \
-    --resource-id "$resource_id" \
-    --http-method "$http_method" \
-    --authorization-type "CUSTOM" \
-    --authorizer-id "$auth_id"
-
-aws apigateway put-integration \
-    --rest-api-id "$api_id" \
-    --resource-id "$resource_id" \
-    --http-method "$http_method" \
-    --type "AWS_PROXY" \
-    --integration-http-method "POST" \
-    --uri "arn:aws:apigateway:eu-west-2:lambda:path/2015-03-31/functions/arn:aws:lambda:eu-west-2:053630928262:function:$lambda_name:\${stageVariables.stageName}/invocations"
-stages=("live" "dev")
-for stage in "${stages[@]}"; do
-    aws lambda add-permission \
-        --function-name "arn:aws:lambda:eu-west-2:053630928262:function:$lambda_name:${stage}" \
-        --source-arn "arn:aws:execute-api:eu-west-2:053630928262:$api_id/*/$http_method/$path" \
-        --principal apigateway.amazonaws.com \
-        --statement-id Allow-API_Invoke-Access \
-        --action lambda:InvokeFunction
-done
-
-# Create lambda methods
 http_method="POST"
 path="login"
 resource_id=$login_resource
@@ -248,6 +219,43 @@ for stage in "${stages[@]}"; do
         --statement-id Allow-API_Invoke-Access \
         --action lambda:InvokeFunction
 done
+
+
+# S3 integration
+http_method="PUT"
+path="{id}"
+resource_id=$image_id_resource
+lambda_name="${API_NAME}_image_id_${http_method}"
+aws apigateway put-method \
+    --rest-api-id "$api_id" \
+    --resource-id "$resource_id" \
+    --http-method "$http_method" \
+    --authorization-type "CUSTOM" \
+    --authorizer-id "$auth_id"
+s3roleARN=$(aws iam create-role \
+    --role-name "${API_NAME}s3PutRole" \
+    --path "/${API_NAME}/" \
+    --assume-role-policy-document "{\"Version\": \"2012-10-17\",\"Statement\": [{\"Sid\": \"\",\"Effect\": \"Allow\",\"Principal\": {\"Service\": [\"apigateway.amazonaws.com\"]},\"Action\": [\"sts:AssumeRole\"]}]}" \
+    --query "Role.Arn" \
+    --output "text"
+)
+s3policyARN=$(aws iam create-policy \
+    --policy-name "${API_NAME}-apigateway-put-s3" \
+    --policy-document "{\"Version\": \"2012-10-17\",\"Statement\": [{\"Sid\": \"APIGWPutAllow\",\"Effect\": \"Allow\",\"Action\": \"s3:PutObject\",\"Resource\": \"arn:aws:s3:::${API_NAME}/*\"}]}" \
+    --query "Policy.Arn" \
+    --output "text"
+)
+aws iam attach-role-policy \
+    --role-name "${API_NAME}s3PutRole" \
+    --policy-arn "${s3policyARN}"
+aws apigateway put-integration \
+    --rest-api-id "$api_id" \
+    --resource-id "$resource_id" \
+    --http-method "$http_method" \
+    --credentials "${s3roleARN}"\
+    --type "AWS" \
+    --integration-http-method "PUT" \
+    --uri "arn:aws:apigateway:eu-west-2:${API_NAME}.s3:path/${API_NAME}/{context.authorizer.artist}/{method.request.path.id}"
 
 aws apigateway create-deployment \
     --rest-api-id "$api_id" \
