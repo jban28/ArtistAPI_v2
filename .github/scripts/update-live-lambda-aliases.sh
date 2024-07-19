e79a35279dcdd4bb7a18f51ea539caab4b570054
jban28 <91541400+jban28@users.noreply.github.com>
2024-07-19 16:56:59 +0100

    fix incorrect loop argument name in update-live-lambda-aliases.sh

--- .github/scripts/update-live-lambda-aliases.sh
+++ .github/scripts/update-live-lambda-aliases.sh
@@ -1,5 +1,5 @@
 func_files=($(ls src/lambda-functions/*.py))
-for file in "${func_files[@]}"
+for func_file in "${func_files[@]}"
 do
     func_name="${func_file/.py/}"
     func_name="artist-api-lambda_${func_name##*/}"
