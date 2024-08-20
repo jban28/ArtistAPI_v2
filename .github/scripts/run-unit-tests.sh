20b7882a9ef70f02b3a09a9779cca9e5e02490de
jban28 <jd.bannister28@gmail.com>
2024-08-20 16:27:51 +0100

    add unit tests before pushing live updates

---
+++ .github/scripts/run-unit-tests.sh
@@ -0,0 +1,2 @@
+python ./tests/test_interface.py $1
+echo "success=$?" >> $GITHUB_OUTPUT
--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,6 +1,7 @@
 from test_classes import *
 import unittest
 import test_lib
+import sys
 
-test_lib.config.TEST_ENV = 'DEV'
+test_lib.config.TEST_ENV = sys.argv.pop(1)
 unittest.main()
