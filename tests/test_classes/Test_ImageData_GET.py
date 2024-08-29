317ed38384b9c5b9e1c7c72805b364aff7752f32
jban28 <jd.bannister28@gmail.com>
2024-08-29 16:46:27 +0100

    add test_valid

--- tests/test_classes/Test_ImageData_GET.py
+++ tests/test_classes/Test_ImageData_GET.py
@@ -8,7 +8,7 @@ class Test_ImageData_GET(TestAPIProxy.TestAPIProxy):
     @classmethod
     def setUpClass(self):
         self.resource = 'image-data'
-        self.method = 'POST'
+        self.method = 'GET'
         self.lambda_handler = staticmethod(__import__(self.resource + '_' + self.method).lambda_handler)
         
     def test_valid(self):
@@ -16,13 +16,32 @@ class Test_ImageData_GET(TestAPIProxy.TestAPIProxy):
             self.invoke_lambda(
                 body=None,
                 url_params=None,
-                query_params=None,
+                query_params={'artist': 'Test_User'},
                 headers=None,
             ),
             {
                 'statusCode': 200,
-                'body': json.dumps('Hello from Lambda!')
-            }   
+                'body': [ 
+                    {
+                        "url": "/bodies/shorts_1",
+                        "name": "Shorts 1",
+                        "srcThumb": "Test_User/thumb/Shorts_1.jpg",
+                        "srcFull": "Test_User/full/Shorts_1.jpg",
+                        "caption": "Shorts 1; pen on paper; 21 x 21 cm, 2020",
+                        "series": "sampleSeries1",
+                        "sequence": 2
+                    },
+                    {
+                        "url":"/bodies/trainer_1",
+                        "name":"Trainer 1",
+                        "srcThumb":"Test_User/thumb/Trainer_1.jpg",
+                        "srcFull":"Test_User/full/Trainer_1.jpg",
+                        "caption":"Trainer 1; pen on paper; 21 x 21 cm, 2020",
+                        "series":"sampleSeries1",
+                        "sequence":1
+                    }
+                ]
+            }
         )
     
 if __name__ == '__main__':
